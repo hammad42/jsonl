79ea24a7ab4bdf6ad824792e4b62aa868ce8c863
hammad <hammadshamim642@gmail.com>
2020-08-19 22:44:11 +0500

    include reading of file

--- json_api_jsonl.py
+++ json_api_jsonl.py
@@ -8,10 +8,10 @@ headers = {
 requestResponse = requests.get("https://api.tiingo.com/iex/?tickers=aapl,spy&token=7d1986e9b07d97f737a1645459a8453acfacac35", headers=headers)
 
 #print(type(requestResponse.json()))
-print(requestResponse.json())
+#print(requestResponse.json())
 a=str(requestResponse.json())
-print("#####################")
-print(a)
+#print("#####################")
+#print(a)
 
 '''
 f=open("stock.json","r")
@@ -27,7 +27,13 @@ f.close() #convert string into json or string into dictionary
 
 with jsonlines.open('stock.jsonl', 'w') as writer:
 
+
     writer.write(a)
+
+
     writer.close()
+f=open('stock.jsonl')
+k=f.read()
+print(k)
      
 
